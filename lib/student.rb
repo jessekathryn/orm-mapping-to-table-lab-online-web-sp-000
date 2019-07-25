da697b0160196088871050c3ba2582ee69ebaa5e
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-25 19:51:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ attr_accessor :id, :name, :grade
   
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS student (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade INTEGER
@@ -21,14 +21,14 @@ attr_accessor :id, :name, :grade
   
   def self.drop_table
     sql =  <<-SQL 
-      DROPS TABLE student
+      DROPS TABLE students
         SQL
     DB[:conn].execute(sql) 
   end
   
   def save
     sql = <<-SQL
-      INSERT INTO student (name, grade) 
+      INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
  
